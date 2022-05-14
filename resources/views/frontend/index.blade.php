cb7b78760677f54e1cf63184c6b035898669e30a
yoyoypy <55522331+yoyoypy@users.noreply.github.com>
2022-05-14 09:37:18 +0700

    default image

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -37,7 +37,7 @@
                                 <div class="product">
                                     <div class="product-image">
                                         @foreach ($product->galleries as $gallery)
-                                            <a href="{{ route('product', $product->slug ) }}" title="{{ $product->name }} product image"><img style="max-height: 400px;overflow: hidden;" alt="{{ $product->name }} product image" src="{{ Storage::url($gallery->url) }}">
+                                            <a href="{{ route('product', $product->slug ) }}" title="{{ $product->name }} product image"><img style="max-height: 400px;overflow: hidden;" alt="{{ $product->name }} item image" src="{{ Storage::url($gallery->url) }} ?? data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==">
                                             </a>
                                         @endforeach
                                         <div class="product-overlay">
